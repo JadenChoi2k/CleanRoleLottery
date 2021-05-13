916b197d9ab5e63ade1e61872aad46c6e58a44d5
JadenChoi2k <45772707+JadenChoi2k@users.noreply.github.com>
2021-05-13 20:11:58 +0900

    Update code.js

--- code.js
+++ code.js
@@ -4,3 +4,20 @@ function printDay() {
 	clock.innerHTML = currentDate.toString();
 	setTimeout("printDay()",1000);
 }
+
+function slide(pos)
+{
+	const wrap = document.querySelector(".slide");
+	const target = wrap.children[0];
+	const len = target.children.length;
+	const height = target.clientHeight;
+	
+	target.style.cssText = `width:calc(100% * ${len});transition:1s;`;
+  	Array.from(target.children).forEach(ele => ele.style.cssText = `width:calc(100% / ${len});`);
+	
+	target.style.marginTop = `${-(pos+1)%len*height}px`;
+}
+
+let pos=0;
+
+window.onload = function () { slide() };
